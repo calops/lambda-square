ebb71b526a6e88524f44409d2107d7e20a666fac
Rémi Labeyrie <calops@tocards.net>
2025-01-08 02:06:50 +0100

    starting up

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
+import cadquery as cq
 import cqkit as kit
 
 
 def make_outer_chassis():
     path = [
-        ("start", {"position": (0.0, 10.0), "direction": 90.0, "width": 0.5}),
-        ("line", {"length": 2.0}),
-        ("arc", {"radius": 2.0, "angle": -90.0}),
-        ("line", {"length": 10.0}),
-        ("arc", {"radius": 2.0, "angle": -90.0}),
-        ("line", {"length": 10.0}),
-        ("arc", {"radius": 2.0, "angle": -90.0}),
-        ("line", {"length": 10.0}),
-        ("arc", {"radius": 2.0, "angle": -90.0}),
-        ("line", {"length": 2.0}),
+        ("start", {"position": (0, 10), "direction": 90, "width": 0.5}),
+        ("line", {"length": 2}),
+        ("arc", {"radius": 2, "angle": -90}),
+        ("line", {"length": 10}),
+        ("arc", {"radius": 2, "angle": -90}),
+        ("line", {"length": 10}),
+        ("arc", {"radius": 2, "angle": -90}),
+        ("line", {"length": 10}),
+        ("arc", {"radius": 2, "angle": -90}),
+        ("line", {"length": 2}),
     ]
 
     ribbon = kit.Ribbon("XY", path)
     return ribbon.render().extrude(10)
 
 
+def make_fan(workplane: cq.Workplane, diameter: float):
+    # Just a simple cube
+    return workplane.box(diameter, diameter, 24)
+
+
 outer_chassis = make_outer_chassis()
+fan = make_fan(cq.Workplane("XY"), 120)
